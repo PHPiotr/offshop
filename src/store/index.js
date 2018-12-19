4ddffd172006a9101ed1cc4eb557c84dabc24cd7
Piotr Kowalski <piet.kowalski@gmail.com>
2018-12-19 01:05:49 +0100

    Hot reload reducers

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,28 @@ import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
+const devEnv = process.env.NODE_ENV === 'development';
+
 const configureStore = (initialState) => {
     const middleware = [];
 
-    if (process.env.NODE_ENV === 'development') {
+    if (devEnv) {
         middleware.push(logger);
     }
 
-    return createStore(
+    const createdStore = createStore(
         rootReducer,
         initialState,
         composeWithDevTools(applyMiddleware(...middleware))
     );
+
+    if(devEnv) {
+        if(module.hot) {
+            module.hot.accept("../reducers", () => createdStore.replaceReducer(require("../reducers").default));
+        }
+    }
+
+    return createdStore;
 };
 
 export const store = configureStore();
