7f6270d58eb351737872e25f07841a1b074293a9
Piotr Kowalski <piet.kowalski@gmail.com>
2019-01-22 23:59:28 +0100

    Setup fallback if no window.Intl

--- src/index.js
+++ src/index.js
@@ -12,22 +12,34 @@ import store from './store';
 import messages from './messages';
 import {flattenMessages} from './utils';
 
-addLocaleData([...en, ...pl]);
+if (!window.Intl) {
+    Promise.all([
+        import('intl'),
+        import('intl/locale-data/jsonp/en.js'),
+        import('intl/locale-data/jsonp/pl.js'),
+    ]).then(() => runApp());
+} else {
+    runApp();
+}
 
-let locale = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage || 'en-US';
+function runApp() {
+    addLocaleData([...en, ...pl]);
 
-ReactDOM.render(
-    <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
-        <Provider store={store}>
-            <Router>
-                <App/>
-            </Router>
-        </Provider>
-    </IntlProvider>,
-    document.getElementById('root')
-);
+    let locale = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage || 'en-US';
+
+    ReactDOM.render(
+        <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+            <Provider store={store}>
+                <Router>
+                    <App/>
+                </Router>
+            </Provider>
+        </IntlProvider>,
+        document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+    serviceWorker.register();
+}
