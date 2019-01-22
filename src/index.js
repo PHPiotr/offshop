da1b3d759629ed6835be093fc5f0b1509b2e6749
Piotr Kowalski <PHPiotr@users.noreply.github.com>
2019-01-23 00:03:56 +0100

    Set up react-intl (#3)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {addLocaleData, IntlProvider} from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import pl from 'react-intl/locale-data/pl';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
+import messages from './messages';
+import {flattenMessages} from './utils';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App/>
-        </Router>
-    </Provider>,
-    document.getElementById('root')
-);
+if (!window.Intl) {
+    Promise.all([
+        import('intl'),
+        import('intl/locale-data/jsonp/en.js'),
+        import('intl/locale-data/jsonp/pl.js'),
+    ]).then(() => runApp());
+} else {
+    runApp();
+}
+
+function runApp() {
+    addLocaleData([...en, ...pl]);
+
+    let locale = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage || 'en-US';
+
+    ReactDOM.render(
+        <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+            <Provider store={store}>
+                <Router>
+                    <App/>
+                </Router>
+            </Provider>
+        </IntlProvider>,
+        document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+    serviceWorker.register();
+}
---
+++ src/messages.js
@@ -0,0 +1,16 @@
+export default {
+    'en-GB': {
+        products: {
+            product_added_to_cart: 'Product has been added to the cart.',
+            continue_shopping: 'Continue shopping',
+            go_to_cart: 'Go to cart',
+        },
+    },
+    'pl-PL': {
+        products: {
+            product_added_to_cart: 'Produkt został dodany do koszyka.',
+            continue_shopping: 'Kontynuuj zakupy',
+            go_to_cart: 'Przejdź do koszyka',
+        },
+    },
+};
