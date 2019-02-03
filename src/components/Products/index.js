d94412089c9bd223ccd251d7062d2268cfa14c5b
Piotr Kowalski <piet.kowalski@gmail.com>
2019-02-03 02:47:57 +0100

    Load images from api on products page

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -39,7 +39,7 @@ function ProductsGridList(props) {
                     <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
                         <GridListTile className={classes.gridListTitle}>
                             <img
-                                src={require(`../../../public/images/${product.img}`)}
+                                src={`${process.env.REACT_APP_API_HOST}/images/products/${product.img}`}
                                 alt={product.name}
                                 className={classes.image}
                             />
