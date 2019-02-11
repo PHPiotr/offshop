f533207e107654cd6054d19e1ea615f7dbdf68a0
Piotr Kowalski <PHPiotr@users.noreply.github.com>
2019-02-11 01:49:26 +0100

    Sync products after order created, product added (#5) (#6)

--- src/actions/cart/index.js
+++ src/actions/cart/index.js
@@ -1,11 +1,16 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const DECREMENT_IN_CART = 'DECREMENT_IN_CART';
+export const DELETE_FROM_CART = 'DELETE_FROM_CART';
 
 export const addToCart = (item, quantity = 1) => ({
     type: ADD_TO_CART,
     payload: { item, quantity },
 });
-export const removeFromCart = (item, quantity = 1) => ({
-    type: REMOVE_FROM_CART,
+export const decrementInCart = (item, quantity = 1) => ({
+    type: DECREMENT_IN_CART,
     payload: { item, quantity },
+});
+export const deleteFromCart = itemId => ({
+    type: DELETE_FROM_CART,
+    payload: { itemId },
 });
