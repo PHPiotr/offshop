0c144f595f06552cfbef3ebb884ddfa400b0986c
Piotr Kowalski <piet.kowalski@gmail.com>
2019-03-05 01:07:05 +0100

    Refactor viewing products

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -37,9 +37,9 @@ function ProductsGridList(props) {
             <GridList cellHeight={`auto`}>
                 {products.map(product => {
 
-                    const productQuantity = product.quantity;
-                    const productInCart = cart.products[product._id] || {quantity: 0};
-                    const canAddToCart = productQuantity - productInCart.quantity > 0;
+                    const productQuantity = product.stock;
+                    const productInCart = cart.products[product._id] || {stock: 0};
+                    const canAddToCart = productQuantity - productInCart.stock > 0;
 
                     return (
                         <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
@@ -52,7 +52,7 @@ function ProductsGridList(props) {
                                 <GridListTileBar
                                     className={classes.gridListTileBar}
                                     title={product.name}
-                                    subtitle={<span>{product.price} zł</span>}
+                                    subtitle={<span>{product.unitPrice} zł</span>}
                                     actionIcon={
                                         <IconButton
                                             id={product._id}
