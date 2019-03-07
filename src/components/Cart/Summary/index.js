325309242bb24c647072155c00bf963499dc2022
Piotr Kowalski <piet.kowalski@gmail.com>
2019-03-07 15:14:27 +0100

    Base delivery cost on a products quantity not units

--- src/components/Cart/Summary/index.js
+++ src/components/Cart/Summary/index.js
@@ -73,7 +73,7 @@ const CartSummary = props => {
                                 0
                             ) +
                                 (props.currentDeliveryMethod.unitPrice > 0
-                                    ? cart.units *
+                                    ? cart.quantity *
                                       props.currentDeliveryMethod.unitPrice
                                     : 0)).toFixed(2)} zł`}
                         </Typography>
