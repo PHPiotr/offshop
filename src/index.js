fef2b79ea992a774f31145d847c7265774e5a8ca
Piotr Kowalski <piet.kowalski@gmail.com>
2019-05-06 02:02:59 +0200

    Service worker fails

--- src/index.js
+++ src/index.js
@@ -41,5 +41,5 @@ function runApp() {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-    serviceWorker.register();
+    //serviceWorker.register();
 }
