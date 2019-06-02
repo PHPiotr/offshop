3223427562c4454a469a23b7fefa93ef5563c450
Piotr Kowalski <piet.kowalski@gmail.com>
2019-06-02 23:04:04 +0200

    No obvious message when cart is empty

--- src/containers/Cart/index.js
+++ src/containers/Cart/index.js
@@ -2,34 +2,8 @@ import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
 import CartProducts from '../../components/Cart/Products';
 import CartSummary from '../../components/Cart/Summary';
-import Paper from '@material-ui/core/Paper/Paper';
-import withStyles from '@material-ui/core/styles/withStyles';
-import {injectIntl, FormattedMessage} from 'react-intl';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import {injectIntl} from 'react-intl';
 import {getDeliveryMethodsIfNeeded} from '../../actions/deliveryMethods';
-
-const styles = theme => ({
-    paper: {
-        marginTop: theme.spacing.unit * 3,
-        marginBottom: theme.spacing.unit * 3,
-        padding: theme.spacing.unit * 2,
-        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-            marginTop: 0,
-            marginBottom: 0,
-            padding: theme.spacing.unit * 3,
-        },
-        width: '100%',
-    },
-    buttons: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-    },
-    button: {
-        marginTop: theme.spacing.unit * 3,
-        marginLeft: theme.spacing.unit,
-    },
-});
 
 class Cart extends Component {
 
@@ -39,17 +13,7 @@ class Cart extends Component {
 
     render() {
         if (!this.props.products.length) {
-            return (
-                <Fragment>
-                    <Paper className={this.props.classes.paper}>
-                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                            <Typography variant="subheading">
-                                <FormattedMessage id="cart.empty"/>
-                            </Typography>
-                        </Grid>
-                    </Paper>
-                </Fragment>
-            );
+            return null;
         }
         return (
             <Fragment>
@@ -73,4 +37,4 @@ const mapDispatchToProps = dispatch => ({
     }
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(withStyles(styles)(Cart)));
+export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(Cart));
