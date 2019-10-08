e3b69696d7f65cb8476223072dcb6689ad10f3eb
Piotr Kowalski <piet.kowalski@gmail.com>
2019-10-08 15:41:27 +0200

    Typo

--- src/components/Checkout/BuyerDeliveryForm/index.js
+++ src/components/Checkout/BuyerDeliveryForm/index.js
@@ -18,7 +18,7 @@ const renderTextField = ({input, label, meta, ...custom}) => (
 
 const validateRequired = value => typeof value === 'string' && value.trim() ? undefined : 'To pole jest wymagane';
 const validateEmail = value => value && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? undefined : 'Niepoprawny email';
-s
+
 const useStyles = makeStyles(theme => ({
     form: {
         paddingLeft: theme.spacing(1),
