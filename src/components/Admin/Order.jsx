048983f968103f60f87736eb8199e311f397bae1
Piotr Kowalski <piet.kowalski@gmail.com>
2019-10-10 15:56:20 +0200

    Display more info on order view page

--- src/components/Admin/Order.jsx
+++ src/components/Admin/Order.jsx
@@ -1,30 +1,46 @@
 import React, {Fragment, useState} from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
 import {Helmet} from 'react-helmet';
+import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {makeStyles} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import CancelIcon from '@material-ui/icons/Cancel';
+import DeleteIcon from '@material-ui/icons/Delete';
+import RefundIcon from '@material-ui/icons/MoneyOff';
+import Tooltip from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
+import RequestHandler from '../../containers/RequestHandler';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction/ListItemSecondaryAction';
+import Divider from '@material-ui/core/Divider';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
+import Dialog from '../Dialog';
 import {
     getOrderIfNeeded,
     cancelOrderIfNeeded,
     deleteOrderIfNeeded,
     refundOrderIfNeeded
 } from '../../actions/admin/order';
-import Card from '@material-ui/core/Card';
-import {makeStyles} from '@material-ui/core';
-import CardHeader from '@material-ui/core/CardHeader';
-import Typography from '@material-ui/core/Typography';
-import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-import CancelIcon from '@material-ui/icons/Cancel';
-import DeleteIcon from '@material-ui/icons/Delete';
-import RefundIcon from '@material-ui/icons/MoneyOff';
-import CardActions from '@material-ui/core/CardActions';
-import Tooltip from '@material-ui/core/Tooltip';
-import Button from '@material-ui/core/Button';
-import Dialog from '../Dialog';
-import RequestHandler from '../../containers/RequestHandler';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
+    list: {
+        width: '100%',
+    },
+    listItem: {
+        paddingLeft: theme.spacing(1),
+        paddingRight: theme.spacing(1),
+    },
+    nested: {
+        paddingLeft: theme.spacing(3),
+    },
+    secondaryAction: {
+        right: 0,
+    },
     card: {
         maxWidth: `800px`,
         margin: '0 auto',
@@ -82,6 +98,26 @@ const Order = props => {
         }
     };
 
+    const [detailsOpen, setDetailsOpen] = useState(true);
+    const handleDetailsClick = () => {
+        setDetailsOpen(!detailsOpen);
+    };
+
+    const [productsOpen, setProductsOpen] = useState(false);
+    const handleProductsClick = () => {
+        setProductsOpen(!productsOpen);
+    };
+
+    const [buyerOpen, setBuyerOpen] = useState(false);
+    const handleBuyerClick = () => {
+        setBuyerOpen(!buyerOpen);
+    };
+
+    const [deliveryOpen, setDeliveryOpen] = useState(false);
+    const handleDeliveryClick = () => {
+        setDeliveryOpen(!deliveryOpen);
+    };
+
     const canRefundOrder = canRefund(order);
     const handleRefundOrderClick = () => {
         if (canRefundOrder) {
@@ -101,42 +137,202 @@ const Order = props => {
             {() => (
                 <Fragment>
                     <Helmet>
-                        <title>{order.description}</title>
+                        <title>{`Zamówienie ${order.extOrderId}`}</title>
                     </Helmet>
-                    <Card className={classes.card}>
-                        <CardHeader
-                            title={order.description}
-                            subheader={new Date(order.localReceiptDateTime || order.orderCreateDate).toLocaleString('pl')}
-                        />
-                        <CardContent>
-                            <Typography component="p">
-                                {order.orderId}
-                            </Typography>
-                            <Typography component="p">
-                                {order.status}
-                            </Typography>
-                        </CardContent>
-                        <CardActions className={classes.actions}>
-                            {canCancelForStatus(order.status) && (<Tooltip title="Anuluj zamówienie">
-                                <IconButton aria-label="Anuluj zamówienie" onClick={handleCancelOrderClick}>
-                                    <CancelIcon color="error"/>
-                                </IconButton>
-                            </Tooltip>)}
-                            {canDeleteForStatus(order.status) && (<Tooltip title="Usuń zamówienie">
-                                <IconButton aria-label="Usuń zamówienie" onClick={handleDeleteOrderClick}>
-                                    <DeleteIcon color="error"/>
-                                </IconButton>
-                            </Tooltip>)}
-                            <Tooltip
-                                title={canRefundOrder ? 'Zwróć zamówienie' : 'Nie można wykonać zwrotu zamówienia'}>
-                                <IconButton
-                                    aria-label={canRefundOrder ? 'Zwróć zamówienie' : 'Nie można wykonać zwrotu zamówienia'}
-                                    onClick={handleRefundOrderClick}>
-                                    <RefundIcon color={canRefundOrder ? 'error' : 'disabled'}/>
-                                </IconButton>
-                            </Tooltip>
-                        </CardActions>
-                    </Card>
+                    <List
+                        className={classes.list}
+                        disablePadding
+                    >
+                        <ListItem className={classes.listItem}>
+                            <ListItemText
+                                primary={order.extOrderId}
+                            />
+                            <ListItemSecondaryAction className={classes.secondaryAction}>
+                                {canCancelForStatus(order.status) && (<Tooltip title="Anuluj zamówienie">
+                                    <IconButton aria-label="Anuluj zamówienie" onClick={handleCancelOrderClick}>
+                                        <CancelIcon color="error"/>
+                                    </IconButton>
+                                </Tooltip>)}
+                                {canDeleteForStatus(order.status) && (<Tooltip title="Usuń zamówienie">
+                                    <IconButton aria-label="Usuń zamówienie" onClick={handleDeleteOrderClick}>
+                                        <DeleteIcon color="error"/>
+                                    </IconButton>
+                                </Tooltip>)}
+                                {canRefundOrder && (<Tooltip
+                                    title="Wykonaj zwrot zamówienia">
+                                    <IconButton
+                                        aria-label="Wykonaj zwrot zamówienia"
+                                        onClick={handleRefundOrderClick}>
+                                        <RefundIcon color="error"/>
+                                    </IconButton>
+                                </Tooltip>)}
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                        <Divider/>
+                        <ListItem button onClick={handleDetailsClick} className={classes.listItem}>
+                            <ListItemText
+                                primary="Szczegóły"
+                            />
+                            {detailsOpen ? <ExpandLess/> : <ExpandMore/>}
+                        </ListItem>
+                        <Collapse in={detailsOpen} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={order.status}
+                                        secondary="Status"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={(new Date(order.localReceiptDateTime || order.orderCreateDate)).toLocaleString('pl', {
+                                            dateStyle: 'short',
+                                            timeStyle: 'short'
+                                        })}
+                                        secondary="Data"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={`${(order.totalAmount / 100).toFixed(2)} zł`}
+                                        secondary="Kwota z dostawą"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={`${(order.totalWithoutDelivery / 100).toFixed(2)} zł`}
+                                        secondary="Kwota bez dostawy"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={`${(order.totalWeight / 100).toFixed(2)} kg`}
+                                        secondary="Waga"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={order.extOrderId}
+                                        secondary="Nr zamówienia widoczny dla klienta"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={order.orderId}
+                                        secondary="Nr zamówienia w PayU (niewidoczny dla klienta)"
+                                    />
+                                </ListItem>
+                                {order.properties.map(({name, value}) => (
+                                    <ListItem className={classes.nested}>
+                                        <ListItemText
+                                            primary={value}
+                                            secondary={name}
+                                        />
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Collapse>
+                        <Divider/>
+                        <ListItem button onClick={handleProductsClick} className={classes.listItem}>
+                            <ListItemText
+                                primary="Produkty"
+                            />
+                            {productsOpen ? <ExpandLess/> : <ExpandMore/>}
+                        </ListItem>
+                        <Collapse in={productsOpen} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                {(order.products || []).map(({name, quantity, unitPrice}) => (
+                                    <ListItem key={name} className={classes.nested}>
+                                        <ListItemText
+                                            primary={`${name} (${quantity} szt.)`}
+                                            secondary={`${(unitPrice / 100).toFixed(2)} zł / szt`}
+                                        />
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Collapse>
+                        <Divider/>
+                        <ListItem button onClick={handleBuyerClick} className={classes.listItem}>
+                            <ListItemText
+                                primary="Kupujący"
+                            />
+                            {buyerOpen ? <ExpandLess/> : <ExpandMore/>}
+                        </ListItem>
+                        <Collapse in={buyerOpen} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={order.buyer.firstName}
+                                        secondary="Imię"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={order.buyer.lastName}
+                                        secondary="Nazwisko"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={order.buyer.email}
+                                        secondary="Adres e-mail"
+                                    />
+                                </ListItem>
+                                <ListItem className={classes.nested}>
+                                    <ListItemText
+                                        primary={order.buyer.phone}
+                                        secondary="Telefon"
+                                    />
+                                </ListItem>
+                            </List>
+                        </Collapse>
+                        {order.buyer.delivery && (
+                            <Fragment>
+                                <Divider/>
+                                <ListItem button onClick={handleDeliveryClick} className={classes.listItem}>
+                                    <ListItemText
+                                        primary="Dostawa"
+                                    />
+                                    {deliveryOpen ? <ExpandLess/> : <ExpandMore/>}
+                                </ListItem>
+                                <Collapse in={deliveryOpen} timeout="auto" unmountOnExit>
+                                    <List component="div" disablePadding>
+                                        <ListItem className={classes.nested}>
+                                            <ListItemText
+                                                primary={`${order.deliveryMethod.name} (${(order.deliveryMethod.unitPrice / 100).toFixed(2)} zł / kg)`}
+                                                secondary="Wybrana opcja"
+                                            />
+                                        </ListItem>
+                                        <ListItem className={classes.nested}>
+                                            <ListItemText
+                                                primary={order.buyer.delivery.recipientName}
+                                                secondary="Odbiorca"
+                                            />
+                                        </ListItem>
+                                        <ListItem className={classes.nested}>
+                                            <ListItemText
+                                                primary={order.buyer.delivery.postalCode}
+                                                secondary="Kod pocztowy"
+                                            />
+                                        </ListItem>
+                                        <ListItem className={classes.nested}>
+                                            <ListItemText
+                                                primary={order.buyer.delivery.city}
+                                                secondary="Miasto"
+                                            />
+                                        </ListItem>
+                                        <ListItem className={classes.nested}>
+                                            <ListItemText
+                                                primary={order.buyer.delivery.street}
+                                                secondary="Ulica"
+                                            />
+                                        </ListItem>
+                                    </List>
+                                </Collapse>
+                            </Fragment>
+                        )}
+
+                    </List>
                     <Dialog
                         title={`Anulować zamówienie?`}
                         content={order.description}
@@ -174,7 +370,13 @@ const Order = props => {
 };
 
 const mapStateToProps = state => ({
-    order: state.adminOrder.data[state.adminOrder.id] || {},
+    order: state.adminOrder.data[state.adminOrder.id] || {
+        buyer: {
+            delivery: {},
+        },
+        deliveryMethod: {},
+        properties: [],
+    },
     isFetching: state.adminOrder.isFetching,
 });
 const mapDispatchToProps = {
