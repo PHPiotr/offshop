c12648d2f6ca69c85467a5263c0bbb301cdd2740
Piotr Kowalski <piet.kowalski@gmail.com>
2020-02-05 00:27:24 +0100

    should show error notification if fetching pay methods fail

--- src/modules/Checkout/components/PayMethods.jsx
+++ src/modules/Checkout/components/PayMethods.jsx
@@ -1,52 +1,69 @@
 import React, {Fragment, useState, useEffect} from 'react';
-import {withStyles} from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
+import {connect} from 'react-redux';
+import {makeStyles} from '@material-ui/core/styles';
+import {Divider, List, ListItem, Typography} from '@material-ui/core';
 import PayByLink from './PayByLink';
 import PayuExpress from './PayuExpress';
 import GooglePay from './GooglePay';
 import {authorize} from '../../../api/payu';
 import {getRequestPrivate} from '../../../api';
+import {showNotification} from '../../../actions/notification';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     listItem: {
         paddingLeft: theme.spacing(1),
         paddingRight: theme.spacing(1),
     },
-});
-
+}));
 const PayMethods = props => {
 
-    const {classes} = props;
+    const classes = useStyles();
     const [payByLinksMethods, setPayByLinksMethods] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        authorize().then(({data: {access_token: accessToken}}) => {
-            getRequestPrivate(accessToken)('/pay-methods')
-                .then(({data: {payByLinks}}) => setPayByLinksMethods(payByLinks))
-                .catch(e => console.error(e));
-        }).catch(e => console.error(e));
+        (async () => {
+            try {
+                const {data: {access_token: accessToken}} = await authorize();
+                const {data: {payByLinks}} = await getRequestPrivate(accessToken)('/pay-methods');
+                setPayByLinksMethods(payByLinks);
+            } catch (e) {
+                setError(e);
+                props.showNotification({
+                    message: e.message,
+                    variant: 'error',
+                });
+            }
+        })();
     }, [payByLinksMethods.length]);
 
+    if (error) {
+        return null;
+    }
+
     return (
-        <List disablePadding>
-            <ListItem className={classes.listItem}>
-                <PayuExpress />
-            </ListItem>
-            <ListItem className={classes.listItem}>
-                <GooglePay />
-            </ListItem>
-            {payByLinksMethods.map(tile => (
-                <Fragment key={tile.value}>
-                    <ListItem className={classes.listItem}>
-                        <PayByLink {...tile}/>
-                    </ListItem>
-                    <Divider />
-                </Fragment>
-            ))}
-        </List>
+        <>
+            <Typography variant="subtitle1" gutterBottom className={classes.title}>
+                Wybierz metodę płatności
+            </Typography>
+            <List disablePadding>
+                <ListItem className={classes.listItem}>
+                    <PayuExpress/>
+                </ListItem>
+                <ListItem className={classes.listItem}>
+                    <GooglePay/>
+                </ListItem>
+                {payByLinksMethods.map(tile => (
+                    <Fragment key={tile.value}>
+                        <ListItem className={classes.listItem}>
+                            <PayByLink {...tile}/>
+                        </ListItem>
+                        <Divider/>
+                    </Fragment>
+                ))}
+            </List>
+        </>
     );
 };
 
-export default withStyles(styles)(PayMethods);
+export default connect(null, {showNotification})(PayMethods);
